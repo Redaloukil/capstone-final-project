a3c97d328d07d02e0a8884863d0f2fa122afd0ef
Redaloukil <loukil.red@gmail.com>
2020-09-15 21:13:45 +0100

    set jenkins

--- backend/app.py
+++ backend/app.py
@@ -1,18 +1,22 @@
 import flask
 from flask import Flask
 from flask import jsonify
+from flask_cors import CORS, cross_origin
 
 import json
 
 app = Flask(__name__)
 
 @app.route('/')
+@cross_origin(supports_credentials=True)
 def hello_world():
-    return jsonify(message="Helllo world")
+    return jsonify(message="Hello world")
+
 
 @app.route('/<username>')
+@cross_origin(supports_credentials=True)
 def show_user_profile(username):
-    return jsonify(message="good choice" , username=ume)
+    return jsonify(message="good choice" , username=username)
 
 
-app.run('0.0.0.0' , 8000)
+app.run('localhost' , 8000)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,28 +3,38 @@ import './App.css';
 
 class App extends Component {
   state = {
-    message:'',
-    error:'',
+    message: '',
+    error: '',
+  };
+
+  componentDidMount() {
+    fetch('http://localhost:8000/')
+      .then((res) => {
+        return res.json();
+      })
+      .then((res) => {
+        this.setState({ message: res.message });
+      })
+      .catch((err) => {
+        this.setState({ error: 'Something went wrong' });
+      });
   }
-	componentDidMount() {
-		fetch('localhost:8000/').then((res) => { 
-      this.setState({message:res});
-    }).catch((err) => {
-      this.setState({error:err});
-    });
-	}
-	render() {
-		return (
-			<div className="App">
-				<header className="App-header">
-					<p>Reda doing some Javascript</p>
-					<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-						Learn React
-					</a>
-				</header>
-			</div>
-		);
-	}
+
+  render() {
+    const { message, error } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>{message || null}</p>
+          {error ? (
+            <div className="alert alert-primary" role="alert">
+              {error}
+            </div>
+          ) : null}
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
